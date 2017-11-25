8cf6041960451aacaf62a35e8f5fdd83ecfc9d07
L Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2017-11-25 18:24:50 +1000

    Delete spawn_test

--- example/spawn_test/tab.R
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env Rscript
-
-tab = expand.grid(real_t=c("double","float"), rot=c("NRot","Rot"), storage=c("SoA","AoS"),
-type=c("ForceCalculator","ForceIntegrator"));
-tab$storage_flag = c(SoA="RFI_StructureOfArrays", AoS="RFI_ArrayOfStructures")[as.character(tab$storage)]
-tab$stub = paste(tab$type, tab$real_t, tab$rot, tab$storage, sep="_")
-write.csv(tab, file="tab.csv", row.names=FALSE)
